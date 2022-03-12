236d9e1b6c4b67aaeadadb2abc5ab6eafd252fcc
mherrera 2020235 <88869102+MarckHerrera@users.noreply.github.com>
2022-03-12 08:44:23 -0600

    marck
    Carpetas para los Crud, Modelos hechos y Usuarios Crud

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+// IMPORTACION RUTAS
+var usuarioRoutes = require("./src/routes/usuario.routes");
+var productoRoutes = require("./src/routes/producto.routes");
+var categoriaRoutes = require("./src/routes/categoria.routes");
+var facturaRoutes = require("./src/routes/factura.routes");
+
+
+
+
+// MIDDLEWARES
+app.use(express.urlencoded({ extended: false}));
+app.use(express.json());
+
+// CABECERAS
+app.use(cors());
+
+// CARGA DE RUTAS localhost:3000/api/
+app.use('/api', usuarioRoutes, productoRoutes, categoriaRoutes, facturaRoutes);
+
+module.exports = app;
